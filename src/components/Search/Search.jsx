f6b46187e78021298af9affba5fdc99ae4a2707b
syedjunaid7 <99808491+syedjunaid7@users.noreply.github.com>
2024-01-31 16:13:58 +0530

    loader add

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,30 +1,26 @@
+import "./Search.scss";
 import React, { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import "./Search.scss";
 import { useNavigate } from "react-router-dom";
 import { goToProduct } from "../../store/cartSlice";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 export default function Search({ inputValue, setInputValue }) {
   const [searchedData, setSearchData] = useState([]);
   const [isSearchBoxVisible, setIsSearchBoxVisible] = useState(false);
+  const [isLoader, setIsLoader] = useState(false);
   const searchBoxRef = useRef(null);
   const { data: products, status } = useSelector((state) => state.product);
-  
-  useEffect(() => {
-    const filterData = products.filter((item) => {
-      return item.title.toLowerCase().includes(inputValue.toLowerCase());
-    });
-    if (inputValue) {
-      setSearchData(filterData);
-      setIsSearchBoxVisible(true);
-    } else {
-      setSearchData([]);
-      setIsSearchBoxVisible(false);
-    }
-  }, [inputValue, products]);
-
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const debounceFunc = (filterData) => {
+    setTimeout(() => {
+      setSearchData(filterData);
+    }, 1000);
+  };
+
   const handleGoTo = (product) => {
     dispatch(goToProduct(product));
     navigate("/productfull");
@@ -50,21 +46,41 @@ export default function Search({ inputValue, setInputValue }) {
     };
   }, []);
 
+  useEffect(() => {
+    const filterData = products.filter((item) => {
+      return item.title.toLowerCase().includes(inputValue.toLowerCase());
+    });
+    if (inputValue) {
+      setIsSearchBoxVisible(true);
+      debounceFunc(filterData);
+    } else {
+      setSearchData([]);
+      setIsSearchBoxVisible(false);
+    }
+  }, [inputValue, products]);
   return (
     <div
       className={isSearchBoxVisible ? "searchBox" : "hide"}
       ref={searchBoxRef}
     >
-      {searchedData.map((product) => (
-        <div
-          className="searchBoxImg"
-          key={product.id}
-          onClick={() => handleGoTo(product)}
-        >
-          <img src={product.image} className="searchImg" alt={product.title} />
-          <p>{product.title}</p>
-        </div>
-      ))}
+      {searchedData.length ? (
+        searchedData.map((product) => (
+          <div
+            className="searchBoxImg"
+            key={product.id}
+            onClick={() => handleGoTo(product)}
+          >
+            <img
+              src={product.image}
+              className="searchImg"
+              alt={product.title}
+            />
+            <p>{product.title}</p>
+          </div>
+        ))
+      ) : (
+        <Skeleton count={4} />
+      )}
     </div>
   );
 }
